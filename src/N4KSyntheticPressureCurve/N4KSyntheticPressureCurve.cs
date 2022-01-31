ecdb48c313490e217400bc8837bf576777213ac7
Michael Herman (Trusted Digital Web) <mwherman@parallelspace.net>
2022-01-31 07:32:51 -0700

    New N4KSyntehticPressureCurve class

---
+++ src/N4KSyntheticPressureCurve/N4KSyntheticPressureCurve.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N4KSyntheticPressureCurve
+{
+    public class N4KSyntheticPressureCurve
+    {
+        public const int MILLI = 1000;
+        public const int D1MSMAX = 10 * MILLI;
+        public const int D2MSMAX = 30 * MILLI;
+        public const int D3MSMAX = 30 * MILLI;
+        public const int D4MSMAX = 30 * MILLI;
+        public const int D5MSMAX = 5 * MILLI;
+
+        public const int PEAKMIN = 0;
+        public const int PEAKMAX = 9;
+
+        private int d1ms, d2ms, d3ms, d4ms, d5ms; // milliseconds
+        private int t0ms, t1ms, t2ms, t3ms, t4ms, t5ms; // milliseconds
+        private int d1s, d2s, d3s, d4s, d5s; // seconds
+        private int t0s, t1s, t2s, t3s, t4s, t5s; // seconds - t5s = total width of the curve
+        private int peak; // peak height of the curve
+        private int medianms, medians; // weigthed median/center of curve in milliseconds and seconds
+
+        public N4KSyntheticPressureCurve(int d1msactual, int d234msactual, int peakactual)
+        {
+            int d2msestimate = (int)Math.Round((double)d234msactual * 3 / 8);
+            int d3msestimate = (int)Math.Round((double)d234msactual * 4 / 8);
+            int d4msestimate = (int)Math.Round((double)d234msactual * 1 / 8);
+
+            Initialize(d1msactual, d2msestimate, d3msestimate, d4msestimate, peakactual);
+        }
+
+        public N4KSyntheticPressureCurve(int d1msactual, int d2msactual, int d3msactual, int d4msactual, int peakactual)
+        {
+            Initialize(d1msactual, d2msactual, d3msactual, d4msactual, peakactual);
+        }
+
+        private void Initialize(int d1msactual, int d2msactual, int d3msactual, int d4msactual, int peakactual)
+        { 
+            d1ms = d1msactual;
+            d2ms = d2msactual;
+            d3ms = d3msactual;
+            d4ms = d4msactual;
+            d5ms = (d2ms + d3ms + d4ms) / 5;
+            peak = peakactual;
+            if (peak == -1) peak = (int)Math.Round((double)PEAKMAX * (double)(d2ms + d3ms + d4ms) / (double)(D2MSMAX + D3MSMAX + D4MSMAX));
+
+            d1s = (int)Math.Round((double)d1ms / MILLI);
+            d2s = (int)Math.Round((double)d2ms / MILLI);
+            d3s = (int)Math.Round((double)d3ms / MILLI);
+            d4s = (int)Math.Round((double)d4ms / MILLI);
+            d5s = (d2s + d3s + d4s) / 5;
+
+            t0s = 0;
+            t1s = t0s + d1s;
+            t2s = t1s + d2s;
+            t3s = t2s + d3s;
+            t4s = t3s + d4s;
+            t5s = t4s = d5s;
+            medians = t2s + (4 * d3s - 2 * d2s + 2 * d4s) / 8; 
+
+            t0ms = 0;
+            t1ms = t0ms + d1ms;
+            t2ms = t1ms + d2ms;
+            t3ms = t2ms + d3ms;
+            t4ms = t3ms + d4ms;
+            t5ms = t4ms + d5ms;
+            medianms = t2ms + (4 * d3ms - 2 * d2ms + 2 * d4ms) / 8;
+        }
+
+        public int TotalWidth() { return t5s; }
+        public int TotalDuration() { return t5ms; }
+        public int Peak() { return peak; }
+        public int MedianS() { return medians; }
+        public int MedianMs() { return medianms; }
+
+        public int Coverage()
+        {
+            int pressCoverage = 0; foreach (int pressure in GetD2PressCurve()) pressCoverage += pressure;
+            int sustainCoverage = 0; foreach (int pressure in GetD3SustainCurve()) sustainCoverage += pressure;
+            int releaseCoverage = 0; foreach (int pressure in GetD4ReleaseCurve()) releaseCoverage += pressure;
+            int totalCoverage = pressCoverage + sustainCoverage + releaseCoverage;
+
+            return totalCoverage;
+        }
+
+        public int[] GetD1ApproachCurve()
+        {
+            int[] curve = new int[d1s];
+            for (int i = 0; i < curve.Length; i++) curve[i] = 0;
+            return curve;
+        }
+
+        public int[] GetD2PressCurve()
+        {
+            int[] curve = GetScaledLeftCurve(peak, d2s);
+            return curve;
+        }
+
+        public int[] GetD3SustainCurve()
+        {
+            int[] curve = new int[d3s];
+            for (int i = 0; i < curve.Length; i++) curve[i] = peak;
+            return curve;
+        }
+
+        public int[] GetD4ReleaseCurve()
+        {
+            int[] curve = GetScaledLeftCurve(peak, d4s);
+
+            for (int i = 0; i < d4s / 2; i++)
+            {
+                int temp = curve[i];
+                curve[i] = curve[d4s - 1 - i];
+                curve[d4s - 1 - i] = temp;
+            }
+
+            return curve;
+        }
+
+        public int[] GetD5RecoveryCurve()
+        {
+            int[] curve = new int[d5s];
+            for (int i = 0; i < curve.Length; i++) curve[i] = 0;
+            return curve;
+        }
+
+        private int[] GetScaledLeftCurve(int peak, int duration)
+        {
+            int[] result = new int[duration];
+
+            //Console.WriteLine("peak, duration: " + peak.ToString() + " " + duration.ToString());
+
+            double a = 0;
+            for (int i = 0; i < duration; i++)
+            {
+                double y = System.Math.Sin(a);
+                int iy = (int)System.Math.Round(y * peak);
+                result[i] = iy;
+                //Console.WriteLine(i.ToString() + "\t" + a.ToString() + "\t" + y.ToString() + "\t" + curve[i].ToString());
+                a += (Math.PI / 2) / duration;
+            }
+
+            return result;
+        }
+    }
+}
--- src/N4KSyntheticPressureCurve/Program.cs
+++ src/N4KSyntheticPressureCurve/Program.cs
@@ -8,113 +8,123 @@ namespace N4KSyntheticPressureCurve
 {
     internal class Program
     {
-        const int MILLI = 1000;
-        const int D1MAX = 10 * MILLI;
-        const int D2MAX = 30 * MILLI;
-        const int D3MAX = 30 * MILLI;
-        const int D4MAX = 30 * MILLI;
-        const int D5MAX = 5 * MILLI;
-
-        const int IPMIN = 1;
-        const int IPMAX = 10;
-
         static bool autoPeak = false;
 
         static void Main(string[] args)
         {
-            int d1, d2, d3, d4, d5;
-            int m, t0, t1, t2, t3, t4, t5;
-            int ip, id1, id2, id3, id4, id5;
-            int im, it0, it1, it2, it3, it4, it5;
-            int totalCoverage, leftCoverage, sustainCoverge, rightCoverage;
+            int d1ms = 0, d2ms = 0, d3ms = 0, d4ms = 0, d5ms = 0;
+            int medianms, t0ms, t1ms, t2ms, t3ms, t4ms, t5ms;
+            int peak = 0, d1s = 0, d2s = 0, d3s = 0, d4s = 0, d5s = 0;
+            int medians, t0s, t1s, t2s, t3s, t4s, t5s;
 
-            ip = IPMAX * 3 / 4;
-            d1 = 5 * MILLI + SomeNoise();
-            d2 = 10 * MILLI + SomeNoise();
-            d3 = 30 * MILLI + SomeNoise();
-            d4 = 10 * MILLI + SomeNoise();
-            d5 = D5MAX + SomeNoise();
-
-            char ch = '-';
+            char ch = 'I';
             while (ch != 'q')
             {
                 switch(ch)
                 {
+                    case 'I':
+                        {
+                            peak = N4KSyntheticPressureCurve.PEAKMAX * 3 / 4;
+                            d1ms = 5 * N4KSyntheticPressureCurve.MILLI + SomeNoise();
+                            d2ms = 10 * N4KSyntheticPressureCurve.MILLI + SomeNoise();
+                            d3ms = 30 * N4KSyntheticPressureCurve.MILLI + SomeNoise();
+                            d4ms = 10 * N4KSyntheticPressureCurve.MILLI + SomeNoise();
+                            d5ms = N4KSyntheticPressureCurve.D5MSMAX + SomeNoise();
+                            break;
+                        }
                     case '=': { autoPeak = !autoPeak; break; }
-                    case '^': case 'V': { ip += 1; autoPeak = false;  if (ip > IPMAX) { ip = IPMAX; Console.Beep(); } break; }
-                    case 'v': { ip -= 1; autoPeak = false;  if (ip < IPMIN) { ip = IPMIN;      Console.Beep(); } break; }
-                    case '1': { ip = 1; autoPeak = false; break; }
-                    case '2': { ip = 2; autoPeak = false; break; }
-                    case '3': { ip = 3; autoPeak = false; break; }
-                    case '4': { ip = 4; autoPeak = false; break; }
-                    case '5': { ip = 5; autoPeak = false; break; }
-                    case '6': { ip = 6; autoPeak = false; break; }
-                    case '7': { ip = 7; autoPeak = false; break; }
-                    case '8': { ip = 8; autoPeak = false; break; }
-                    case '9': { ip = 9; autoPeak = false; break; }
-                    case '0': { ip = 10; autoPeak = false; break; }
-                    case 'a': { d1 += SomeNoise(); if (d1 > D1MAX) { d1 = D1MAX - SomeNoise(); Console.Beep(); } break; }
-                    case 'A': { d1 -= SomeNoise(); if (d1 < MILLI) { d1 = MILLI + SomeNoise(); Console.Beep(); } break; }
-                    case 'p': { d2 += SomeNoise(); if (d2 > D2MAX) { d2 = D2MAX - SomeNoise(); Console.Beep(); } break; }
-                    case 'P': { d2 -= SomeNoise(); if (d2 < MILLI) { d2 = MILLI + SomeNoise(); Console.Beep(); } break; }
-                    case 's': { d3 += SomeNoise(); if (d3 > D3MAX) { d3 = D3MAX - SomeNoise(); Console.Beep(); } break; }
-                    case 'S': { d3 -= SomeNoise(); if (d3 < MILLI) { d3 = MILLI + SomeNoise(); Console.Beep(); } break; }
-                    case 'r': { d4 += SomeNoise(); if (d4 > D4MAX) { d4 = D4MAX - SomeNoise(); Console.Beep(); } break; }
-                    case 'R': { d4 -= SomeNoise(); if (d4 < MILLI) { d4 = MILLI + SomeNoise(); Console.Beep(); } break; }
+                    case '^': case 'V': 
+                        { 
+                            peak += 1; autoPeak = false;  
+                            if (peak > N4KSyntheticPressureCurve.PEAKMAX) { peak = N4KSyntheticPressureCurve.PEAKMAX; Console.Beep(); }
+                            break; 
+                        }
+                    case 'v': 
+                        { peak -= 1; autoPeak = false;  
+                            if (peak < N4KSyntheticPressureCurve.PEAKMIN) { peak = N4KSyntheticPressureCurve.PEAKMIN; Console.Beep(); } 
+                            break; 
+                        }
+                    case '1': { peak = 1; autoPeak = false; break; }
+                    case '2': { peak = 2; autoPeak = false; break; }
+                    case '3': { peak = 3; autoPeak = false; break; }
+                    case '4': { peak = 4; autoPeak = false; break; }
+                    case '5': { peak = 5; autoPeak = false; break; }
+                    case '6': { peak = 6; autoPeak = false; break; }
+                    case '7': { peak = 7; autoPeak = false; break; }
+                    case '8': { peak = 8; autoPeak = false; break; }
+                    case '9': { peak = 9; autoPeak = false; break; }
+                    case '0': { peak = 10; autoPeak = false; break; }
+                    case 'a': 
+                        { 
+                            d1ms += SomeNoise(); 
+                            if (d1ms > N4KSyntheticPressureCurve.D1MSMAX) { d1ms = N4KSyntheticPressureCurve.D1MSMAX - SomeNoise(); Console.Beep(); } 
+                            break; 
+                        }
+                    case 'A': { d1ms -= SomeNoise(); if (d1ms < N4KSyntheticPressureCurve.MILLI) { d1ms = N4KSyntheticPressureCurve.MILLI + SomeNoise(); Console.Beep(); } break; }
+                    case 'p': 
+                        { 
+                            d2ms += SomeNoise(); 
+                            if (d2ms > N4KSyntheticPressureCurve.D2MSMAX) { d2ms = N4KSyntheticPressureCurve.D2MSMAX - SomeNoise(); Console.Beep(); } 
+                            break; 
+                        }
+                    case 'P': { d2ms -= SomeNoise(); if (d2ms < N4KSyntheticPressureCurve.MILLI) { d2ms = N4KSyntheticPressureCurve.MILLI + SomeNoise(); Console.Beep(); } break; }
+                    case 's': 
+                        { 
+                            d3ms += SomeNoise(); 
+                            if (d3ms > N4KSyntheticPressureCurve.D3MSMAX) { d3ms = N4KSyntheticPressureCurve.D3MSMAX - SomeNoise(); Console.Beep(); }
+                            break; 
+                        }
+                    case 'S': { d3ms -= SomeNoise(); if (d3ms < N4KSyntheticPressureCurve.MILLI) { d3ms = N4KSyntheticPressureCurve.MILLI + SomeNoise(); Console.Beep(); } break; }
+                    case 'r': 
+                        { 
+                            d4ms += SomeNoise(); 
+                            if (d4ms > N4KSyntheticPressureCurve.D4MSMAX) { d4ms = N4KSyntheticPressureCurve.D4MSMAX - SomeNoise(); Console.Beep(); }
+                            break; 
+                        }
+                    case 'R': { d4ms -= SomeNoise(); if (d4ms < N4KSyntheticPressureCurve.MILLI) { d4ms = N4KSyntheticPressureCurve.MILLI + SomeNoise(); Console.Beep(); } break; }
                     default: { break; }
 
                 }
 
-                if (autoPeak) ip = (int)Math.Round((double)IPMAX * (double)(d2 + d3 + d4) / (double)(D2MAX + D3MAX + D4MAX));
+                d5ms = (d2ms + d3ms + d4ms) / 5;
 
-                t0 = 0;
-                t1 = t0 + d1;
-                t2 = t1 + d2;
-                t3 = t2 + d3;
-                t4 = t3 + d4;
+                t0ms = 0;
+                t1ms = t0ms + d1ms;
+                t2ms = t1ms + d2ms;
+                t3ms = t2ms + d3ms;
+                t4ms = t3ms + d4ms;
+                t5ms = t4ms + d5ms;
 
-                d5 = (d2 + d3 + d4) / 5;
-                t5 = t4 + d5;
+                if (autoPeak) peak = -1;
 
-                m = t2 + (4 * d3 - 2 * d2 + 2 * d4) / 8;
+                var synthCurve = new N4KSyntheticPressureCurve(d1ms, d2ms, d3ms, d4ms, peak);
+                //var synthCurve = new N4KSyntheticPressureCurve(d1ms, d2ms + d3ms + d4ms, peak);
 
-                // Normalize values for plotting the curve
-                it0 = (int)Math.Round((double)t0 / MILLI);
-                it1 = (int)Math.Round((double)t1 / MILLI);
-                it2 = (int)Math.Round((double)t2 / MILLI);
-                it3 = (int)Math.Round((double)t3 / MILLI);
-                it4 = (int)Math.Round((double)t4 / MILLI);
-                it5 = (int)Math.Round((double)t5 / MILLI);
-                id1 = it1 - it0;
-                id2 = it2 - it1;
-                id3 = it3 - it2;    
-                id4 = it4 - it3;    
-                id5 = it5 - it4;
+                int[] approachCurve = synthCurve.GetD1ApproachCurve();
+                int[] pressCurve = synthCurve.GetD2PressCurve();
+                int[] sustainCurve = synthCurve.GetD3SustainCurve();
+                int[] releaseCurve = synthCurve.GetD4ReleaseCurve();
+                int[] recoveryCurve = synthCurve.GetD5RecoveryCurve();
 
-                im = it2 + (4 * id3 - 2 * id2 + 2 * id4) / 8;
+                // Recalculate durations (seconds)
+                d1s = approachCurve.Length;
+                d2s = pressCurve.Length;
+                d3s = sustainCurve.Length;
+                d4s = releaseCurve.Length;
+                d5s = recoveryCurve.Length;
 
-                Console.Clear();
-                Console.WriteLine("BlueToqueTools N4K Synthentic Pressure Curve for a Kiss version 0.1");
-                Console.WriteLine("Time unit: 0.1 millisecond (0.0001 second)");
-                Console.WriteLine();
-                Console.WriteLine("t0=" + t0.ToString().PadRight(10) + "\tAPPROACH (approach " + d1.ToString() + ")\t" + it0.ToString());
-                Console.WriteLine("t1=" + t1.ToString().PadRight(10) + "\tPRESS    (press    " + d2.ToString() + ")\t" + it1.ToString() + " " + id1.ToString());
-                Console.WriteLine("t2=" + t2.ToString().PadRight(10) + "\tSUSTAIN  (sustain  " + d3.ToString() + ")\t" + it2.ToString() + " " + id2.ToString());
-                Console.WriteLine("t3=" + t3.ToString().PadRight(10) + "\tRELEASE  (release  " + d4.ToString() + ")\t" + it3.ToString() + " " + id3.ToString());
-                Console.WriteLine("t4=" + t4.ToString().PadRight(10) + "\tRECOVERY (recovery " + d5.ToString() + ")\t" + it4.ToString() + " " + id4.ToString());
-                Console.WriteLine("t5=" + t5.ToString().PadRight(10) + "\tFINISH                 "             + " \t" + it5.ToString() + " " + id5.ToString());
-                Console.WriteLine();
-                Console.WriteLine("m=" + m.ToString().PadRight(10) +  "\tMEDIAN   |" + "\t" + im.ToString());
-                Console.WriteLine("p=" + ip.ToString().PadRight(10) + "\tPEAK     *" + "\tautoPeak " + autoPeak.ToString());
+                // Recalculate time line values (seconds)
+                t0s = 0;
+                t1s = t0s + d1s;
+                t2s = t1s + d2s;
+                t3s = t2s + d3s;
+                t4s = t3s + d4s;
+                t5s = t4s + d5s;
 
-                //Console.WriteLine();
-                int[] leftPressureCurve = GetScaledLeftCurve(ip, id2);
-                int[] rightPressureCurve = GetScaledRightCurve(ip, id4);
-
-                leftCoverage = 0; foreach (int pressure in leftPressureCurve) leftCoverage += pressure;
-                sustainCoverge = id3 * ip;
-                rightCoverage = 0; foreach (int pressure in rightPressureCurve) rightCoverage += pressure;
-                totalCoverage = id1 + leftCoverage + sustainCoverge + rightCoverage + id5;
+                int totalCoverage = synthCurve.Coverage();
+                medians = synthCurve.MedianS();
+                medianms = synthCurve.MedianMs();
+                peak = synthCurve.Peak();
 
                 // Clear chart
                 char[][] chart = new char[10][];
@@ -125,51 +135,54 @@ namespace N4KSyntheticPressureCurve
                 }
 
                 // Plot synthetic pressure curve
-                for (int i = it0+1; i <= it1; i++) chart[1 - 1][i - 1] = '_'; // APPROACH
-
-                for (int i = 1; i <= id2; i++)
-                {
-                    int y = leftPressureCurve[i - 1];
-                    chart[y - 1][it1 + i - 1] = '/'; // PRESS
-                }
-
-                for (int t = it2+1; t <= it3; t++) chart[ip - 1][t - 1] = '='; // SUSTAIN
+                for (int i = 0; i < approachCurve.Length; i++) chart[approachCurve[i]][t0s + i] = '_'; // APPROACH
+                for (int i = 0; i < pressCurve.Length; i++) chart[pressCurve[i]][t1s + i] = '/'; // PRESS
+                for (int i = 0; i < sustainCurve.Length; i++) chart[sustainCurve[i]][t2s + i] = '='; // SUSTAIN
+                for (int i = 0; i < releaseCurve.Length; i++) chart[releaseCurve[i]][t3s + i] = '\\'; // RELEASE
+                for (int i = 0; i < recoveryCurve.Length; i++) chart[recoveryCurve[i]][t4s + i] = '_'; // RECOVERY
 
                 foreach (var row in chart)
                 {
-                    row[im - 1] = '|'; // PEAK
+                    row[medians] = '|'; // PEAK
                 }
-                chart[ip-1][im - 1] = '*';  // TOP
+                chart[peak][medians] = '*';  // TOP
 
-                for (int i = 1; i <= id4; i++)
-                {
-                    int y = rightPressureCurve[i - 1];
-                    chart[y - 1][it3 + i - 1] = '\\'; // RELEASE
-                }
-
-                for (int t = it4 + 1; t <= it5; t++) chart[1 - 1][t - 1] = '_'; // RECOVERY
+                // Output chart
+                Console.Clear();
+                Console.WriteLine("BlueToqueTools N4K Synthentic Pressure Curve for a Kiss version 0.2");
+                Console.WriteLine("Time unit: 0.1 millisecond (0.0001 second)");
+                Console.WriteLine();
+                Console.WriteLine("t0=" + t0ms.ToString().PadRight(10) + "\tAPPROACH (approach " + d1ms.ToString() + ")\t" + t0s.ToString() + " " + d1s.ToString());
+                Console.WriteLine("t1=" + t1ms.ToString().PadRight(10) + "\tPRESS    (press    " + d2ms.ToString() + ")\t" + t1s.ToString() + " " + d2s.ToString());
+                Console.WriteLine("t2=" + t2ms.ToString().PadRight(10) + "\tSUSTAIN  (sustain  " + d3ms.ToString() + ")\t" + t2s.ToString() + " " + d3s.ToString());
+                Console.WriteLine("t3=" + t3ms.ToString().PadRight(10) + "\tRELEASE  (release  " + d4ms.ToString() + ")\t" + t3s.ToString() + " " + d4s.ToString());
+                Console.WriteLine("t4=" + t4ms.ToString().PadRight(10) + "\tRECOVERY (recovery " + d5ms.ToString() + ")\t" + t4s.ToString() + " " + d5s.ToString());
+                Console.WriteLine("t5=" + t5ms.ToString().PadRight(10) + "\tFINISH                 " + " \t" + t5s.ToString());
+                Console.WriteLine();
+                Console.WriteLine("m=" + medianms.ToString().PadRight(10) + "\tMEDIAN   |" + "\t" + medians.ToString());
+                Console.WriteLine("p=" + peak.ToString().PadRight(10) + "\tPEAK     *" + "\tautoPeak " + autoPeak.ToString());
 
                 // Output left axis and synthetic pressure curve
                 Console.WriteLine();
                 for (int y = 10; y > 0; y--)
                 {
                     Console.Write((y % 10).ToString());
-                    for (int x = 0; x < it5; x++) Console.Write(chart[y-1][x]);
+                    for (int x = 0; x < t5s; x++) Console.Write(chart[y-1][x]);
                     Console.WriteLine();
                 }
 
                 // Output timeline
                 Console.WriteLine();
                 Console.Write(">");
-                for (int t = 1; t <= id1; t++) Console.Write("a");
-                for (int t = 1; t <= id2; t++) Console.Write("p");
-                for (int t = 1; t <= id3; t++) Console.Write("s");
-                for (int t = 1; t <= id4; t++) Console.Write("r");
-                for (int t = 1; t <= id5; t++) Console.Write("v");
+                for (int t = 1; t <= d1s; t++) Console.Write("a");
+                for (int t = 1; t <= d2s; t++) Console.Write("p");
+                for (int t = 1; t <= d3s; t++) Console.Write("s");
+                for (int t = 1; t <= d4s; t++) Console.Write("r");
+                for (int t = 1; t <= d5s; t++) Console.Write("v");
                 Console.WriteLine();
 
                 // Output bottom axis
-                for (int x = 0; x <= it5; x++)
+                for (int x = 0; x <= t5s; x++)
                 {
                     if ((x % 10) == 0)
                     {
@@ -202,13 +215,11 @@ namespace N4KSyntheticPressureCurve
         static Random r = new Random((int)DateTime.Now.Ticks);
         private static int SomeNoise()
         {
-            return r.Next(MILLI);
+            return r.Next(N4KSyntheticPressureCurve.MILLI);
         }
 
         static int[] GetScaledLeftCurve(int peak, int duration)
         {
-            const double PI = 3.14159;
-
             int[] result = new int[duration];
 
             //Console.WriteLine("peak, duration: " + peak.ToString() + " " + duration.ToString());
@@ -220,7 +231,7 @@ namespace N4KSyntheticPressureCurve
                 int iy = (int)System.Math.Round(y * (peak-1)) + 1;
                 result[i] = iy;
                 //Console.WriteLine(i.ToString() + "\t" + a.ToString() + "\t" + y.ToString() + "\t" + result[i].ToString());
-                a += (PI / 2) / duration;
+                a += (Math.PI / 2) / duration;
             }
 
             return result; 
